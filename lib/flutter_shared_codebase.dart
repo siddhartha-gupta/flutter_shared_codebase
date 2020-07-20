f20bb53fc9dcdfa745e56897d0f493f02c52c34a
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2020-07-20 17:58:38 +0530

    testing without services

--- lib/flutter_shared_codebase.dart
+++ lib/flutter_shared_codebase.dart
@@ -3,5 +3,5 @@ library flutter_shared_codebase;
 export 'widget/header_wrapper_widget.dart';
 export 'widget/page_wrapper_widget.dart';
 
-export 'services/SharedPreferencesService.dart';
-export 'services/LocalStorageService.dart';
+// export 'services/SharedPreferencesService.dart';
+// export 'services/LocalStorageService.dart';
