ee08dc6261d699fc77bf75386d33e82e671345e9
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2020-07-20 18:13:55 +0530

    path fixes

--- lib/flutter_shared_codebase.dart
+++ lib/flutter_shared_codebase.dart
@@ -1,7 +1,7 @@
 library flutter_shared_codebase;
 
-export 'widget/header_wrapper_widget.dart';
-export 'widget/page_wrapper_widget.dart';
+export 'src/widget/header_wrapper_widget.dart';
+export 'src/widget/page_wrapper_widget.dart';
 
-// export 'services/SharedPreferencesService.dart';
-// export 'services/LocalStorageService.dart';
+// export 'src/services/SharedPreferencesService.dart';
+// export 'src/services/LocalStorageService.dart';
